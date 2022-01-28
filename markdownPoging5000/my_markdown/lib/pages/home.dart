e8eb1776b30d5a71084a5844c5d162a790414f58
Jeroen van Roest <Jeroenvanroest@gmail.com>
2022-01-28 21:26:38 +0100

    C

--- markdownPoging5000/my_markdown/lib/pages/home.dart
+++ markdownPoging5000/my_markdown/lib/pages/home.dart
@@ -2,7 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
-class HomePage extends StatelessWidget {
+// import '../expansionpanel.dart';
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(
+//           'Home',
+//         ),
+//         centerTitle: true,
+//         elevation: 0.0,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 30.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Row(
+//               children: <Widget>[
+//                 Expanded(
+//                   child: RaisedButton(
+//                     onPressed: () {
+//                       Navigator.push(
+//                           context,
+//                           MaterialPageRoute(
+//                               builder: (BuildContext context) =>
+//                                   Expansionpanel()));
+//                     },
+//                     child: Text('Zuid-afrika'),
+//                   ),
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//----------------------------------------------------
+var file;
+
+class Markfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,10 +54,14 @@ class HomePage extends StatelessWidget {
         title: const Text("Flutter Markdown"),
       ),
       body: FutureBuilder(
-          future: rootBundle.loadString("hello.md"),
+          future: rootBundle.loadString('zuidafrika.md'),
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
             if (snapshot.hasData) {
-              return Markdown(data: snapshot.data!);
+              //test = 'markdown vol';
+              var test = Markdown(data: snapshot.data!);
+              return test;
+            } else {
+              file = 'bestand = null';
             }
 
             return const Center(
@@ -22,4 +70,44 @@ class HomePage extends StatelessWidget {
           }),
     );
   }
+}
+
+//----------------------------------------------------
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_markdown/flutter_markdown.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Homepagina'),
+              bottom: TabBar(
+                tabs: [
+                  Tab(text: 'Startpagina'),
+                  Tab(text: 'Moldavië'),
+                  Tab(text: 'Tanzania'),
+                  Tab(text: 'Roemenië'),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                Center(
+                    child: Text('Welkom op de Livingstone Infoapp!',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                Center(child: Text(file)),
+                Center(child: Text('page 2: Tanzania')),
+                Center(child: Text('page 3: roemenië')),
+              ],
+            )),
+      );
 }
